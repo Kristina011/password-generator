abfff21de76baa233e59ccc88667d08cc24f9bc7
kikiBg <kristinarsic@gmail.com>
2020-04-02 15:11:40 +0200

    password generator create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+  length = 0;
+  lettersIncluded = false;
+  numbersIncluded = false;
+  symbolsIncluded = false;
+  password = '';
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.lettersIncluded = !this.lettersIncluded;
+  }
+
+  onChangeUseNumbers() {
+    this.numbersIncluded = !this.numbersIncluded;
+  }
+
+  onChangeUseSymbols() {
+    this.symbolsIncluded = !this.symbolsIncluded;
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.lettersIncluded) {
+      validChars += letters;
+    }
+    if (this.numbersIncluded) {
+      validChars += numbers;
+    }
+    if (this.symbolsIncluded) {
+      validChars += symbols;
+    }
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
 }
